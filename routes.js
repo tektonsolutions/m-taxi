a8fe503493aa835f7775f6028d948b0cc6d13011
John Rezan Leslie Baguna <jrezan.b@gmail.com>
2016-02-01 14:03:55 +0800

    log in page

--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
 Router.configure({
-  // layoutTemplate:"main",
+  layoutTemplate:"main"
   // loadingTemplate: "loading"
 });
 
-// Router.route("/", {
-//   name: "login",
-//   template: "login",
-//   onBeforeAction: function(){
-//     var currentUser = Meteor.userId();
-//     if(currentUser){
-//       Router.go("");
-//     } else{
-//       this.next();
-//     }
-//   }
-// });
+Router.route("/", {
+  name: "login",
+  template: "login"
+  // onBeforeAction: function(){
+  //   var currentUser = Meteor.userId();
+  //   if(currentUser){
+  //     Router.go("");
+  //   } else{
+  //     this.next();
+  //   }
+  // }
+});
